ca05856ae29fb5b81769542ea74f3ba09e781987
epere01 <emmanuel.perez@toka.mx>
2024-09-11 14:41:10 -0600

    contract IProductRepository added

--- contracts/IProductRepository.cs
+++ contracts/IProductRepository.cs
@@ -1,5 +1,12 @@
 
-public interface IProductRepository {
-    // 
+using MinimalCatalogApi.Models;
 
+namespace MinimalCatalogApi.Contracts {
+    public interface IProductRepository {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(int productId);
+        Task<bool> CreateProduct(UpsertProductDto productToCreate);
+        Task<bool> UpdateProductById(int productId, UpsertProductDto productToUpdate);
+        Task<bool> DeactivateProductById(int productId);
+    }
 }
