2931e9de216c5c5be5a63259c7c49f192637c080
epere01 <emmanuel.perez@toka.mx>
2024-09-11 14:43:38 -0600

    ProductRepository and routing in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,71 @@
+
+using Microsoft.Extensions.DependencyInjection;
+using MinimalCatalogApi.Contracts;
+using MinimalCatalogApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
+
+var productsRepository = new ProductRepository(connectionString!);
 
 app.MapGet("/", () => "Hello World!");
 
 //  PRODUCT ENDPOINTS
 
-// app.MapGet("/products" => );
+app.MapGet("/products", async () =>
+{
+    try
+    {
+        var products = await productsRepository.GetProducts();
+        return Results.Ok(products);
+    }
+    catch (Exception ex)
+    {
+        throw new Exception(ex.Message);
+    }
+});
 
+app.MapGet("/products/{id}", async (int id) =>
+{   
+    try
+    {
+        var product = await productsRepository.GetProductById(id);
+        return Results.Ok(product);    
+    }
+    catch (Exception ex)
+    {
+        throw new Exception(ex.Message);
+    }
+});
+
+app.MapPost("/products", async (UpsertProductDto productToCreate) => {
+    var productCreated = await productsRepository.CreateProduct(productToCreate);
+    if (productCreated) {
+        return Results.Ok("Product has been created successfully");
+    } else {
+        return Results.Problem("Error at creating Product", statusCode:500);
+    }
+});
+
+app.MapPut("/products/{id}", async (int productId, UpsertProductDto productToCreate) => {
+    var productCreated = await productsRepository.UpdateProductById(productId, productToCreate);
+    if (productCreated) {
+        return Results.Ok("Product has been updated successfully");
+    } else {
+        return Results.Problem("Error at updating Product", statusCode: 500);
+    }
+});
+
+app.MapDelete("/products/{id}", async (int productId) => {
+    var productCreated = await productsRepository.DeactivateProductById(productId);
+    if (productCreated) {
+        return Results.Ok("Product has been deactivated successfully");
+    } else {
+        return Results.Problem("Error at deacting Product", statusCode:500);
+    }
+});
 
 app.Run();
--- repository/ProductRepository.cs
+++ repository/ProductRepository.cs
@@ -1,7 +1,124 @@
 
+using Dapper;
+using Microsoft.Data.SqlClient;
+using MinimalCatalogApi.Contracts;
+using MinimalCatalogApi.Models;
 
-public class ProductRepository: IProductRepository {
-// 
+namespace MinimalCatalogApi.Repositories {
+    public class ProductRepository: IProductRepository {
+        
+        private readonly string _connectionString;
+
+        public ProductRepository (string connectionString){
+            _connectionString = connectionString;
+        } 
+
+        public async Task<IEnumerable<Product>>GetProducts(){
+            string query = "SELECT * FROM PRODUCTS";
+            using (var connection = new SqlConnection(_connectionString)){
+                try
+                {
+                    return await connection.QueryAsync<Product>(query);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }   
+
+        public async Task<Product>GetProductById(int productId){
+            string query = @"
+                SELECT * FROM CatalogShema.Products
+                WHERE product_id = @Id
+            ";
+            using (var connection = new SqlConnection(_connectionString)){
+                try
+                {
+                    return await connection.QueryFirstOrDefaultAsync<Product>(query, new { Id = productId });
+                }
+                catch (Exception ex)
+                {   
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+
+        public async Task<bool>CreateProduct(UpsertProductDto productToCreate){
+            string sql = @"
+                INSERT INTO CatalogSchema.Products (
+                    product_name,
+                    product_price
+                ) VALUES(
+                    @ProductName,
+                    @ProductPrice
+                )
+            ";
+            using (var connection = new SqlConnection(_connectionString)){
+                try
+                {
+                    var affectedRows = await connection.ExecuteAsync(sql, new {
+                        ProductName = productToCreate.product_name,
+                        ProductPrice = productToCreate.product_price
+                    });
+
+                    return affectedRows > 0;   
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+        }
+
+        public async Task<bool>UpdateProductById(int productId, UpsertProductDto productToUpdate){
+            string sql = @"
+                UPDATE CatalogSchema.Products
+                SET 
+                    product_name = @ProductName, 
+                    product_price = @ProductPrice
+                WHERE product_id = @ProductId AND 
+            ";
+            using (var connection = new SqlConnection(_connectionString)){
+
+                try
+                {
+                    var affectedRows = await connection.ExecuteAsync(sql, new { 
+                        @ProductName = productToUpdate.product_name, 
+                        @ProductPrice = productToUpdate.product_price 
+                    });
+
+                    return affectedRows > 0;   
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            };
+        }
+
+        public async Task<bool>DeactivateProductById(int productId){
+            string sql = @"
+                UPDATE CatalogSchema.Products
+                SET 
+                    product_active = 0, 
+                WHERE product_id = @ProductId AND product_active = 1; 
+            ";
+            using (var connection = new SqlConnection(_connectionString)){
+                try
+                {
+                    var affectedRows = await connection.ExecuteAsync(sql, new { ProductId = productId });
+                    return affectedRows > 0;   
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+        }
+    }
 }
 
 
